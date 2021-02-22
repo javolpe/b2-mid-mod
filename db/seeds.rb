cfec4f4b5ad20da1f2d3d6abfe02c5ef913663c9
Jacob Volpe <volpe.jacob@gmail.com>
2021-02-22 09:45:02 -0700

    Make seed file, migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+merry_go_round = Ride.create!(name: "Merry Go Round", thrill_rating: 3, open: true)
+batcoaster = Ride.create!(name: "Bat Coaster", thrill_rating: 8, open: true)
+splash_mountain = Ride.create!(name: "Splash Mountain", thrill_rating: 5, open: false)
+dueling_dragons = Ride.create!(name: "Dueling Dragons", thrill_rating: 9, open: true)
+
+james = Mechanic.create!(name: "James Holden", years_experience: 4)
+naomi = Mechanic.create!(name: "Naomi Nagata", years_experience: 11)
+amos = Mechanic.create!(name: "Amos Burton", years_experience: 8)
